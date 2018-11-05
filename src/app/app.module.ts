fee9aa743fa0c362217ac65303e3376d6b1f3ea1
Karl Noelle <karl.noelle@gmail.com>
2018-11-04 21:57:03 -0600

    runpurpose angular kickoff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { EventsComponent } from './events/events.component';
+import { RootComponent } from './root/root.component';
+import { AdminComponent } from './admin/admin.component';
+
+const appRoutes: Routes = [
+  { path: '', component: RootComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'admin', component: AdminComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    EventsComponent,
+    RootComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
